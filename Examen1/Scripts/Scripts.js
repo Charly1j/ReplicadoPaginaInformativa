f1cae70cf8dec52509aec00ccf77e5257f3ccb1d
Charly1j <146923633+Charly1j@users.noreply.github.com>
2024-05-29 18:02:37 -0400

    Add files via upload

---
+++ Examen1/Scripts/Scripts.js
@@ -0,0 +1,16 @@
+function mostrarAlerta(){
+    onclick
+    alert("Pag. in coming soon")
+}
+function form(){
+    onclick
+    prompt("Ingrese su correo para mas informacion:")
+}
+function texto(){
+    onclick
+    var newtex =
+
+        InputEvent("escriba su texto")
+    
+    console.log("elemento cliqueado")
+}
